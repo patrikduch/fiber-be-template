5881c1a342f33337cf98ff7acd98eac4ba09df96
patrikduch <duchpatrik@icloud.com>
2026-03-30 13:09:06 +0200

    feat: user model improvement

--- models/user.go
+++ models/user.go
@@ -5,15 +5,15 @@ import (
 )
 
 type User struct {
-    ID                   uuid.UUID `json:"id"`
-    Name                 string    `json:"username"`
-    Email                string    `json:"email"`
-    NormalizedEmail      string    `json:"normalizedEmail"`
-    Password             string    `json:"-"`
-    EmailConfirmed       bool      `json:"emailConfirmed"`
-    PhoneNumberConfirmed bool      `json:"phoneNumberConfirmed"`
-    TwoFactorEnabled     bool      `json:"twoFactorEnabled"`
-    LockoutEnabled       bool      `json:"lockoutEnabled"`
-    AccessFailedCount    int       `json:"accessFailedCount"`
-    Role                 *Role     `json:"role,omitempty"` // 👈 Add this line
+	ID                   uuid.UUID `json:"id"`
+	Name                 string    `json:"username"`
+	Email                string    `json:"email"`
+	NormalizedEmail      string    `json:"normalizedEmail"`
+	Password             string    `json:"-"`
+	EmailConfirmed       bool      `json:"emailConfirmed"`
+	PhoneNumberConfirmed bool      `json:"phoneNumberConfirmed"`
+	TwoFactorEnabled     bool      `json:"twoFactorEnabled"`
+	LockoutEnabled       bool      `json:"lockoutEnabled"`
+	AccessFailedCount    int       `json:"accessFailedCount"`
+	Role                 *Role     `json:"role,omitempty"`
 }
